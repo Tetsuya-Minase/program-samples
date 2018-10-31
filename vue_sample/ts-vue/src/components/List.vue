0092adc170901a370bd2fbcba501801041ab0561
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2018-11-01 00:34:46 +0900

    vueサンプル追加

---
+++ vue_sample/ts-vue/src/components/List.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+    <input type="text" id="data" v-model="todo">
+    <button @click="addTodo()">登録</button>
+    <ul id="list">
+        <li v-for="todoItem in todoList">
+            {{todoItem}}
+        </li>
+    </ul>
+</div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+// @Component デコレータはクラスが Vue コンポーネントであることを示します
+@Component({})
+export default class ListComponent extends Vue {
+  @Prop() public addItems!: string[];
+  private todo: string = '';
+  private todoList: string[] = [];
+
+  constructor() {
+      super();
+      this.todoList = this.todoList.concat(this.addItems);
+  }
+
+  private addTodo() {
+      this.todoList.push(this.todo);
+      this.todo = '';
+  }
+}
+</script>
+<style lang="scss" scoped>
+ul {
+    margin-left: auto;
+    margin-right: auto;
+    width: 300px;
+    li:nth-child(even) {
+        color: blue;
+    }
+}
+#list {
+    text-align: left
+}
+</style>
+
---
+++ vue_sample/ts-vue/src/views/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="todo">
+    <ListComponent :addItems="addList"/>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ListComponent from "@/components/List.vue";
+
+@Component({
+  components: {
+    ListComponent,
+  },
+})
+export default class Todo extends Vue {
+    public addList = ['test1', 'test2'];
+}
+</script>
