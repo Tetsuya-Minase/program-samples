ac7d440a727842dafd1e5f3205b4e3e0e6d81e90
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2019-10-13 23:06:30 +0900

    feat: angular + jest sample create.

---
+++ ng-jest/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
