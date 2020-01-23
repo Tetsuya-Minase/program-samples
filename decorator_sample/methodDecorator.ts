a192d6994af5abe51fd9c1db688b4cc078fa5144
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-01-23 23:38:47 +0900

    method decorator done.

--- decorator_sample/methodDecorator.ts
+++ decorator_sample/methodDecorator.ts
@@ -16,3 +16,50 @@ class SampleClassMethod {
 }
 
 console.log(new SampleClassMethod().greet('test'));
+
+function greetDecorator(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  descriptor.value = function () {
+    return `こんにちは。${arguments[0]}`;
+  };
+}
+
+class MethodDecoratorGreeter {
+  @greetDecorator
+  greet(name: string) {
+    return `Hi! ${name}`;
+  }
+}
+console.log(new MethodDecoratorGreeter().greet('Tom'));
+
+function reflectDecorator(type: 'original' | 'change') {
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    // 後で実行したいので退避しておく
+    const originalMethod = descriptor.value;
+    switch(type) {
+      case 'original':
+        descriptor.value = function() {
+          return Reflect.apply(originalMethod, this, arguments);
+        }
+        break;
+      case 'change':
+        descriptor.value = function() {
+          return '変更した';
+        }
+    }
+  }
+}
+
+class ReflectSample {
+  @reflectDecorator('original')
+  noCange() {
+    return '元のメソッド';
+  }
+
+  @reflectDecorator('change')
+  change() {
+    return '元のメソッド';
+  }
+}
+const reflectSample = new ReflectSample();
+console.log(`noChange: ${reflectSample.noCange()}`);
+console.log(`change  : ${reflectSample.change()}`);
