00e530cfca6fda267d55cdc323dd6c144ffe4207
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-04-26 22:09:26 +0900

    effect追加

--- hooks-sample/src/App.tsx
+++ hooks-sample/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { StateComponent } from './samples/State';
+import { EffectComponent } from './samples/Effect';
 
 function App() {
   return (
     <>
       <StateComponent />
+      <EffectComponent />
     </>
   );
 }
