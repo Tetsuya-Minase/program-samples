fd0dbfecddb04ff45329fb78f5dbf30ad32fcd09
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-06-29 23:48:46 +0900

    全体微修正

--- realtime-db-react/src/firebase/FirebaseAuthComponent.tsx
+++ realtime-db-react/src/firebase/FirebaseAuthComponent.tsx
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { firebase } from './firebase';
 
 export const FirebaseAuthComponent: React.FC = () => {
-  const { loggedin } = useFirebaseLogin();
+  const loggedin = useFirebaseLogin();
   if (!loggedin) {
-    return <button onClick={() => signInWithRedirect()}>ログイン</button>;
+    return <button onClick={() => signInWithPopup()}>ログイン</button>;
   }
   return <button onClick={() => signOut()}>ログアウト</button>;
 }
-const signInWithRedirect = () => {
+const signInWithPopup = () => {
   const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-  return firebase.auth().signInWithRedirect(googleAuthProvider);
+  return firebase.auth().signInWithPopup(googleAuthProvider);
 }
 const signOut = () => {
   return firebase.auth().signOut();
@@ -18,10 +18,10 @@ const signOut = () => {
 const useFirebaseLogin = () => {
   const [loggedin, setLoggedin] = useState(false);
   useEffect(() => {
-    firebase.auth().onAuthStateChanged(async user => {
+    firebase.auth().onAuthStateChanged(user => {
       setLoggedin(!!user);
     });
   }, []);
 
-  return { loggedin };
+  return loggedin;
 };
