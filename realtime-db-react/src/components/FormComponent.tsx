0d03fcb5a3702d9e1457f18784a6270a1517823a
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-06-30 22:27:55 +0900

    削除追加

--- realtime-db-react/src/components/FormComponent.tsx
+++ realtime-db-react/src/components/FormComponent.tsx
@@ -1,18 +1,19 @@
 import React, { useState, ChangeEvent } from 'react';
-import { useRegisterData, useUpdateData } from '../firebase/firebaseDB';
+import { useRegisterData, useUpdateData, useDelteData } from '../firebase/firebaseDB';
 
 export const FormComponent: React.FC = () => {
   const registerData = useRegisterData();
   const updateData = useUpdateData();
+  const deleteData = useDelteData();
 
   const [keyData, setKeyData] = useState<string>('');
   const [valueData, setValueData] = useState<string>('');
-
 
   return <>
     <label>Key: <input placeholder="key" onChange={(event: ChangeEvent<HTMLInputElement>) => setKeyData(event.target.value)}/></label>
     <label>Value: <input placeholder="value" onChange={(event: ChangeEvent<HTMLInputElement>) => setValueData(event.target.value)}/></label>
     <button onClick={() => registerData({[keyData]: valueData})}>登録</button>
     <button onClick={() => updateData({[keyData]: valueData})}>更新</button>
+    <button onClick={() => deleteData()}>全消し</button>
   </>;
 }
