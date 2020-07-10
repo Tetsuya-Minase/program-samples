aa62aa3b2a5f8a8f6decb16d0a610c785d65cb71
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-07-11 00:15:16 +0900

    feat: rename. remove unnecessary variable.

--- realtime-db-sample-with-angular/src/app/components/list/list.component.ts
+++ realtime-db-sample-with-angular/src/app/components/list/list.component.ts
@@ -1,27 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FirebaseUsecaseService } from 'firebase-library';
-import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
-  private _items = new BehaviorSubject<any[]>([]);
-  private _sample = new BehaviorSubject<any[]>([]);
+export class ListComponent {
+  private items$ = new BehaviorSubject<any[]>([]);
   constructor(private readonly firebase: FirebaseUsecaseService) {
     firebase.fetchDocumentAll().subscribe(res => {
-      console.log('res: ', res);
-      this._items.next(res);
+      this.items$.next(res);
     }, err => {
       console.log('error', err);
     });
   }
 
-  ngOnInit(): void {
-  }
-
   get items() {
-    return this._items;
+    return this.items$;
   }
 }
