511164bf909508389e20b4c7683b43bb24faf1f4
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-07-13 22:05:51 +0900

    feat: return html.🐥

---
+++ sample.thymeleaf/src/main/java/minase/sample/thymeleaf/intefaces/controller/HelloController.java
@@ -0,0 +1,12 @@
+package minase.sample.thymeleaf.intefaces.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(){
+        return "Hello";
+    }
+}
