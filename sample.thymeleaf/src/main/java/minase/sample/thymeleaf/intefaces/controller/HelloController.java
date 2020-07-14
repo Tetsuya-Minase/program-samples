bb883d957c6b7c0c02952d4cdb3b002f5a14e61a
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-07-14 22:42:46 +0900

    feat: hello page.🐥

--- sample.thymeleaf/src/main/java/minase/sample/thymeleaf/intefaces/controller/HelloController.java
+++ sample.thymeleaf/src/main/java/minase/sample/thymeleaf/intefaces/controller/HelloController.java
@@ -1,12 +1,35 @@
 package minase.sample.thymeleaf.intefaces.controller;
 
+import minase.sample.thymeleaf.intefaces.model.GreetingModel;
+import minase.sample.thymeleaf.intefaces.model.HelloModel;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@Controller()
+@RequestMapping("hello")
 public class HelloController {
-    @GetMapping("hello")
-    public String hello(){
+    @GetMapping()
+    public String hello(Model model){
+        model.addAttribute("helloModel", new HelloModel());
         return "Hello";
+    }
+
+    @PostMapping("/register")
+    public String registerHello(@ModelAttribute final HelloModel body, Model model) {
+        final var greetingModel = new GreetingModel();
+        greetingModel.setName(body.getName());
+        switch (body.getLanguage()) {
+            case "ja":
+                greetingModel.setGreeting("こんにちは！");
+                break;
+            case "en":
+                greetingModel.setGreeting("Hello!");
+                break;
+            default:
+                break;
+        }
+        model.addAttribute("greetingModel", greetingModel);
+        return "Greeting";
     }
 }
---
+++ sample.thymeleaf/src/main/java/minase/sample/thymeleaf/intefaces/model/HelloModel.java
@@ -0,0 +1,9 @@
+package minase.sample.thymeleaf.intefaces.model;
+
+import lombok.Data;
+
+@Data
+public class HelloModel {
+    private String language;
+    private String name;
+}
