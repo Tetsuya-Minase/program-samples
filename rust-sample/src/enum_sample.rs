8e229ba17a0c4ac67f1a697536d26e2f0c9f551b
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-10-12 21:21:48 +0900

    add enum sample 2.

--- rust-sample/src/enum_sample.rs
+++ rust-sample/src/enum_sample.rs
@@ -59,3 +59,37 @@ pub fn enum_sample() {
   // type aliasを介して参照できる
   let add = Operations::Add;
 }
+
+enum Status {
+  Rich,
+  Poor,
+}
+
+enum Work {
+  Civilian,
+  Soldier,
+}
+
+fn enum_use_sample() {
+  // `use`することで絶対名でなくとも使用可能になる。
+  use Status::{Poor, Rich};
+  // `Work`の中の名前をすべて`use`する
+  use Work::*;
+
+  // `use`しているため、`Status::Poor`と書いていることに等しい
+  let status = Poor;
+  // こちらも同様に`Work::Civilian`に等しい
+  let work = Civilian;
+
+  match status {
+      // `use`しているのでスコープを明示していない
+      Rich => println!("The rich have lots of money!"),
+      Poor => println!("The poor have no money..."),
+  }
+
+  match work {
+      // こちらも同じ
+      Civilian => println!("Civilians work!"),
+      Soldier  => println!("Soldiers fight!"),
+  }
+}
