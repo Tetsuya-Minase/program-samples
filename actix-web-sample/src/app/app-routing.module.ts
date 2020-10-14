3d4af0811efdb2e1dfe538fa1ea8972d6c7eaadf
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-10-15 00:49:03 +0900

    クライアント修正。

--- actix-web-sample/src/app/app-routing.module.ts
+++ actix-web-sample/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { SecondPageComponent } from './second-page/second-page.component';
+import { FirstPageComponent } from './first-page/first-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FirstPageComponent},
+  {path: 'second', component: SecondPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- actix-web-sample/src/app/app.module.ts
+++ actix-web-sample/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { FirstPageComponent } from './first-page/first-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondPageComponent,
+    FirstPageComponent
   ],
   imports: [
     BrowserModule,
