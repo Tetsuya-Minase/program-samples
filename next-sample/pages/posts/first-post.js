74cf4541941f44ac4aa5ea191b5388cd2e073c1b
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-11-13 18:34:54 +0900

    feat: ssg and ssr memo.

--- next-sample/pages/posts/first-post.js
+++ next-sample/pages/posts/first-post.js
@@ -2,14 +2,35 @@ import Link from 'next/link';
 import Head from 'next/head';
 
 // 関数名は何でも良いけど、default exportする必要がある
-export default function FirstPost() {
+export default function FirstPost({pageTitle}) {
   return (
     <>
       <Head>
-        <title>First Post</title>
+        <title>{pageTitle}</title>
       </Head>
 
       <h1>First Post</h1>
       <p><Link href="/"><a>Back to home</a></Link></p>
     </>);
 }
+
+// getStaticPropsはdevelop時にはリクエストごとに走るが、production時にはビルド時に実行される
+export async function getStaticProps() {
+  const pageTitle = 'First Post';
+  return {
+    props: {
+      pageTitle
+    }
+  }
+}
+
+// getStaticPropsをexport or なにもexportしないとSSGの挙動になる
+// getServerSidePropsをexportするとSSRになる
+// export async function getServerSideProps() {
+//   const pageTitle = 'First Post';
+//   return {
+//     props:{
+//       pageTitle
+//     }
+//   }
+// }
