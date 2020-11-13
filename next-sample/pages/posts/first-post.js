35e7adab76eb88d64c3e779da84ba6898a1816e5
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-11-13 15:06:13 +0900

    feat: first-post head tag.

--- next-sample/pages/posts/first-post.js
+++ next-sample/pages/posts/first-post.js
@@ -1,8 +1,14 @@
 import Link from 'next/link';
+import Head from 'next/head';
+
 // 関数名は何でも良いけど、default exportする必要がある
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
       <p><Link href="/"><a>Back to home</a></Link></p>
     </>);
