f732604225e0de89a2892f9471731f94346b05ce
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-01-15 21:02:19 +0900

    feat: add scully.

---
+++ nx-scully-sample/scully.nx-scully-sample.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./apps/nx-scully-sample/src",
+  projectName: "nx-scully-sample",
+  outDir: './dist/static',
+  routes: {
+  }
+};
