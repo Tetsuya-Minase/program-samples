e6ed812189dfbd292d8dd895fabc8e43f9b95c86
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-02-08 20:59:49 +0900

    feat: setup and teardown.

---
+++ re-jest/__test__/src/functions/setupSample.test.ts
@@ -0,0 +1,31 @@
+beforeEach(() => {
+  console.log('このファイル内のテストケースの前に実行される');
+});
+describe('setup and teardown sample.', () => {
+  beforeAll(() => {
+    console.log('全テストケースの前に実行される');
+  });
+  afterAll(() => {
+    console.log('全テストケースの後に実行される');
+  });
+  beforeEach(() => {
+    console.log('各テストケースの前に実行される');
+  });
+  afterEach(() => {
+    console.log('各テストケースの後に実行される');
+  });
+
+  it('テスト1', () => {
+    console.log('テスト1');  
+  });
+  it('テスト2', () => {
+    console.log('テスト2');  
+  });
+
+  it('テスト3', () => {
+    console.log('テスト3');  
+  });
+
+});
+
+export {};
