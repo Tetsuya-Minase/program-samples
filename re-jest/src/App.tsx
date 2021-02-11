154a650bbda0db916e5684b8d5b282e0cd93350c
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-02-11 17:34:56 +0900

    feat: add snapshots test.

--- re-jest/src/App.tsx
+++ re-jest/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Logo } from './Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
