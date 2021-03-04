8f9b2aeed773afbae562d73055b8e67345e21402
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-03-04 22:28:52 +0900

    feat: recoil sample

---
+++ recoil-sample/src/components/Counter.tsx
@@ -0,0 +1,34 @@
+import { atom, selector, useRecoilState, useRecoilValue } from "recoil";
+const countState = atom({
+  key: 'count',
+  default: 0
+});
+const selectEvenNumber = selector({
+  key: 'selectEvenNumber',
+  get: ({get}) => {
+    const count = get(countState);
+    return count % 2 === 0 ? 'Yes' : 'No';
+  }
+});
+const useCountClick = () => {
+  const [count, setCount] = useRecoilState<number>(countState);
+  const onClick = () => setCount(count + 1);
+  return <button onClick={onClick}>+1</button>
+}
+const useCheckCount = () => {
+  const [count,] = useRecoilState<number>(countState);
+  const isEven = useRecoilValue(selectEvenNumber);
+  return <div>
+    Question: is {count} even?<br/>
+    Answer: {isEven}
+  </div>
+}
+
+export const Counter = () => {
+  return (
+    <>
+      {useCountClick()}
+      {useCheckCount()}
+    </>
+  )
+}
