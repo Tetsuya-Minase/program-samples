8ca3574d7f5a21687ca28611552d6ad782aa2f23
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-03-12 23:55:39 +0900

    feat: add style and fix label name

--- recoil-sample/src/components/Form/Form.tsx
+++ recoil-sample/src/components/Form/Form.tsx
@@ -1,12 +1,13 @@
+import './Form.css';
 import { DeepMap, FieldError, useForm } from 'react-hook-form';
 import { TodoItem } from '../model/TodoList';
 import { useSubmit } from './FormUseCase';
 const errorMessage = (errors: DeepMap<TodoItem, FieldError>, todoItemKey: keyof TodoItem) => {
   switch (todoItemKey) {
     case 'value':
-      return errors.value ? <span>やることは必須です。</span> : null;
+      return errors.value ? <span className="form__error">やることは必須です。</span> : null;
     case 'completionDate':
-      return errors.completionDate ? <span>完了日は必須です。</span> : null;
+      return errors.completionDate ? <span className="form__error">完了日は必須です。</span> : null;
     default:
       return null;
   }
@@ -15,12 +16,12 @@ const errorMessage = (errors: DeepMap<TodoItem, FieldError>, todoItemKey: keyof 
 export const Form = () => {
   const { register, handleSubmit, errors } = useForm<TodoItem>();  
   const submit = useSubmit();
-  return <form onSubmit={handleSubmit(submit)}>
-    <label>やること：<input name="value" ref={register({ required: true })}></input></label>
+  return <form className="form" onSubmit={handleSubmit(submit)}>
+    <label className="form__item">やること：<input name="value" ref={register({ required: true })}></input></label>
     {errorMessage(errors, 'value')}
-    <label>完了日：<input type="date" name="completionDate" ref={register({ required: true })}></input></label>
+    <label className="form__item">完了日：<input type="date" name="completionDate" ref={register({ required: true })}></input></label>
     {errorMessage(errors, 'completionDate')}
-    <label>完了/未完了<input type="checkbox" name="isCompleted" ref={register({ setValueAs: value => !!value })}></input></label>
+    <label className="form__item">完了済<input type="checkbox" name="isCompleted" ref={register({ setValueAs: value => !!value })}></input></label>
     <button type="submit">登録</button>
   </form>;
 };
