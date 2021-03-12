1c5c9b43221c0a898dc41d2095e04bf71a787137
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-03-13 01:09:52 +0900

    refactor: use useSetRecoilState

--- recoil-sample/src/components/Form/FormUseCase.ts
+++ recoil-sample/src/components/Form/FormUseCase.ts
@@ -1,9 +1,9 @@
-import { useRecoilCallback, useRecoilState } from "recoil";
+import { useRecoilCallback, useSetRecoilState } from "recoil";
 import { TodoItem, TodoItemResponse, todoListState, TodoListState } from "../model/TodoList";
 import { post } from "../usecase/Fetch";
 
 export const useSubmit = () => {
-  const [, setState] = useRecoilState<TodoListState>(todoListState);
+  const setState = useSetRecoilState<TodoListState>(todoListState);
   const submit = useRecoilCallback(({snapshot}) => async (data: TodoItem) => {
     const response = await post<TodoItemResponse, TodoItem>('http://localhost:8080/api/v1/json', data);
     if (response instanceof Error) {
--- recoil-sample/src/components/List/ListUseCase.ts
+++ recoil-sample/src/components/List/ListUseCase.ts
@@ -1,10 +1,10 @@
 import { useEffect } from 'react'
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { get } from '../usecase/Fetch';
 import { todoListState, getTodoListView, TodoListState, TodoListView } from '../model/TodoList';
 
 export const useFetchList = () => {
-  const [, setState] = useRecoilState<TodoListState>(todoListState);
+  const setState = useSetRecoilState<TodoListState>(todoListState);
   useEffect(() => {
     (async () => {
       const result = await get<TodoListState>('http://localhost:8080/api/v1/json');
