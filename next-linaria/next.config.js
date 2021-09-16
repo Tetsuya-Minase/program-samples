726da90baef83ceb262e57f5733d7d65fcd9e097
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-09-16 23:36:00 +0900

    build: add libraries

--- next-linaria/next.config.js
+++ next-linaria/next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withLinaria = require('next-linaria');
+module.exports = withLinaria({
+  // reactStrictMode: true,
+})
