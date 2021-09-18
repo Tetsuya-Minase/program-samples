d3df5b179b37ab7e995b0a0f1e25e8ea28b19841
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-09-18 10:44:37 +0900

    build: add linaria and webpack options

--- next-linaria/next.config.js
+++ next-linaria/next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const withLinaria = require('next-linaria');
 module.exports = withLinaria({
-  // reactStrictMode: true,
+  reactStrictMode: true,
+  linaria: {
+    // linariaのオプションはここに追加
+  },
+  // webpackのオプションはここに追加
+  webpack(config, options) {
+    return config;
+  }
 })
